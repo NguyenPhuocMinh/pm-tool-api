3ec790a4a1c53f8aa71612397e177dcfbee9837e
minhnp <xiuquay55@gmail.com>
2022-10-14 00:31:13 +0700

    setup deploy

---
+++ src/services/sum.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+export default sum;
---
+++ tests/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import sum from '../src/services/sum';
+
+describe('It will test the sum service', () => {
+  it('should test the sum of 2 numbers', (done) => {
+    const num1 = 10;
+    const num2 = 20;
+    const data = sum(num1, num2);
+    expect(data).to.eq(num1 + num2);
+    done();
+  });
+});
