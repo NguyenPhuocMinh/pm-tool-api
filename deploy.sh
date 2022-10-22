974cf2fd9f59cb0242aa27989a26c26816717171
minhnp <xiuquay55@gmail.com>
2022-10-22 10:33:36 +0700

    building

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,9 @@
 # halt if any error occurs
 set -e  
 
-# Acess permission when run .sh file
-sudo chmod +x deploy.sh
+ACCOUNT=$(eval echo "\$${CIRCLE_BRANCH}_NSCONF_ACCOUNT")
 
-# echo "export AWS_ACCESS_KEY_ID=${APP_AWS_ACCESS_KEY}"
-# echo "export AWS_SECRET_ACCESS_KEY=${APP_AWS_SECRET_KEY}"
-
-if [ -z "${APP_AWS_ACCESS_KEY:-}" ]; then
-  echo "You must set APP_AWS_ACCESS_KEY!"
-fi
-
-echo ${APP_AWS_ACCESS_KEY}
-
-echo "Circle branch===$CIRCLE_BRANCH"
-
-echo "AWS_ACCESS_KEY_ID====$AWS_ACCESS_KEY_ID"
-
-echo "AWS_SECRET_ACCESS_KEY====$AWS_ACCESS_KEY_ID"
+echo "export NSCONF_ACCOUNT=${ACCOUNT:-$NSCONF_ACCOUNT}" >> $BASH_ENV
 
 echo "end build"
 
