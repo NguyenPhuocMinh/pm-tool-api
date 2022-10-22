6954cbdd7913c2ba9cb6b4e364c7119b6518c1f5
minhnp <xiuquay55@gmail.com>
2022-10-22 10:53:35 +0700

    building

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,7 @@
 # halt if any error occurs
 set -e  
 
-ACCOUNT=$(eval echo "\$${CIRCLE_BRANCH}_NSCONF_ACCOUNT")
-
-echo "export NSCONF_ACCOUNT=${ACCOUNT:-$NSCONF_ACCOUNT}" >> $BASH_ENV
+echo $1
 
 echo "end build"
 
