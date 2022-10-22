a9d8990efaa5a606a35804d2adf85b58016af247
minhnp <xiuquay55@gmail.com>
2022-10-22 11:02:42 +0700

    building

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,11 @@
 set -e  
 
 echo $1
+echo $2
+
+echo "Login aws...."
+
+aws ecr get-login-password --region $1 | helm registry login --username AWS --password-stdin $2.dkr.ecr.$1.amazonaws.com
 
 echo "end build"
 
