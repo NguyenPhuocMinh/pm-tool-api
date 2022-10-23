4ca12448af761efaa2998e8f9c0a74aeda19f140
minhnp <xiuquay55@gmail.com>
2022-10-23 09:59:02 +0700

    config build cicd circle

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,20 @@ set -e
 
 $APP_AWS_REGION=$1
 $APP_AWS_ACCOUNT_ID=$2
+$APP_HELM_TAG=$3
 
 echo APP_AWS_REGION=$APP_AWS_REGION
 echo APP_AWS_ACCOUNT_ID=$APP_AWS_ACCOUNT_ID
-
-# cd deploy
-
-# helm package pm-tool-api
+echo APP_HELM_TAG=$APP_HELM_TAG
 
 # uninstall pm-tool-api chart
-# helm uninstall pm-tool-api
+helm uninstall pm-tool-api
 
 echo "Login aws...."
 
 aws ecr get-login-password --region $APP_AWS_REGION | helm registry login --username AWS --password-stdin $APP_AWS_ACCOUNT_ID.dkr.ecr.$APP_AWS_REGION.amazonaws.com
 
+# install helm chart from aws ecr
+helm install pm-tool-api oci://$APP_AWS_ACCOUNT_ID.dkr.ecr.$APP_AWS_REGION.amazonaws.com/pm-tool-api --version $APP_HELM_TAG
 
-echo "end build"
-
+echo "End build"
