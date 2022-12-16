1f9ca1fca5af1127edb6ec98c83655b0536bfaf8
minhnp <xiuquay55@gmail.com>
2022-12-17 00:58:51 +0700

    modify build

--- api/404.js
+++ api/404.js
@@ -1,4 +1,6 @@
 module.exports = (req, res) => {
   console.info('req', req);
-  res.status(404).json({ message: 'Not Found' });
+  res
+    .status(404)
+    .json({ message: 'Not Found', req: JSON.stringify(req, null, 2) });
 };
