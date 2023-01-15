06c07c75892249cf60553c0caeec768c33857cf5
minhnp <xiuquay55@gmail.com>
2023-01-15 14:32:59 +0700

    add future team

--- src/conf/profiles.js
+++ src/conf/profiles.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 const profiles = {
-  VERSION: 'v0.2.2',
+  VERSION: 'v0.2.3',
   APP_ENV: process.env.NODE_ENV || 'dev',
   APP_PORT: process.env.APP_PORT || 8080,
   APP_HOST: process.env.APP_HOST || '0.0.0.0',
